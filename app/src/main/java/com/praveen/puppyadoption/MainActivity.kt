d4698a8ab416df3e754ffee6e1cfb3bb9f510ee1
Praveen Rajput <thepraveenrajput@outlook.com>
2021-03-02 13:49:24 +0530

    Initial commit

---
+++ app/src/main/java/com/praveen/puppyadoption/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.praveen.puppyadoption
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.praveen.puppyadoption.ui.theme.PuppyAdoptionTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuppyAdoptionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuppyAdoptionTheme {
+        Greeting("Android")
+    }
+}
